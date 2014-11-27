bfebd0fd4b912bcf7fa2dafd15bc5ad88e24a9ee
Jeff Walker <walker.jeff.d@gmail.com>
2014-11-26 19:44:20 -0500

    set up basic express app

---
+++ web/app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    path = require('path');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+var server = app.listen(app.get('port'), function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
